d387f314911994fa1afd3cf5117421a4646b8ade
Rajat Goel <rajatgoel789@gmail.com>
2016-09-08 22:28:12 +0530

    dt

--- dataTypes.js
+++ dataTypes.js
@@ -4,7 +4,24 @@ var myname = "Rajat Goel";
 var fullname = " Hey, I'm  " + myname + " . \nand i'm " + (decVar + 2);
 var arr = [1, 2, 3, 4];
 var newarr = [1, 2, 3, 4];
+var varTuple = ["Rajat", 10];
+var colorSet;
+(function (colorSet) {
+    colorSet[colorSet["Red"] = 4] = "Red";
+    colorSet[colorSet["Yellow"] = 5] = "Yellow";
+    colorSet[colorSet["Green"] = 6] = "Green";
+})(colorSet || (colorSet = {}));
+;
+var color = colorSet.Green;
+var colorname = colorSet[colorSet.Green];
+function username() {
+    console.log("username method triggered");
+}
 console.log("isDone", isDone);
 console.log("decVar", decVar);
 console.log("myname", myname, "and fullName is \n\n", fullname);
 console.log("arr", arr, "\n\n newarr", newarr);
+console.log("Tuple is ", varTuple);
+console.log("color is", color);
+console.log("colorname is", colorname);
+username();
--- dataTypes.ts
+++ dataTypes.ts
@@ -6,6 +6,21 @@ and i'm ${decVar+2}`;
 let arr:number[]=[1,2,3,4];
 let newarr:Array<number>=[1,2,3,4];
 
+let varTuple:[string,number] = ["Rajat" , 10]
+enum colorSet {Red  =4  , Yellow , Green };
+
+let color:colorSet = colorSet.Green ; 
+let colorname:string = colorSet[colorSet.Green];
+
+let myfullname:any = "Raj";
+
+let nameLength:number = (fullname as string).length;
+
+function username():void {
+	console.log("username method triggered");	
+}
+
+
 console.log("isDone" , isDone) ; 
 
 console.log("decVar" , decVar) ; 
@@ -13,3 +28,12 @@ console.log("decVar" , decVar) ;
 console.log("myname" , myname , "and fullName is \n\n" , fullname) ; 
 
 console.log("arr" , arr , "\n\n newarr" , newarr) ; 
+
+
+console.log("Tuple is " , varTuple);
+
+console.log("color is" , color);
+
+console.log("colorname is" , colorname);
+username();
+console.log("fullname is" , myfullname , "and name length is " ,nameLength);
