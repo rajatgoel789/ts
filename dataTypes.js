034053fd84ccdd18609bd0511e24f227ca21e803
Rajat Goel <rajatgoel789@gmail.com>
2016-09-08 22:03:33 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+var isDone = true;
+var decVar = 23;
+var myname = "Rajat Goel";
+var fullname = " Hey, I'm  " + myname + " . \nand i'm " + (decVar + 2);
+var arr = [1, 2, 3, 4];
+var newarr = [1, 2, 3, 4];
+console.log("isDone", isDone);
+console.log("decVar", decVar);
+console.log("myname", myname, "and fullName is \n\n", fullname);
+console.log("arr", arr, "\n\n newarr", newarr);
---
+++ dataTypes.ts
@@ -0,0 +1,15 @@
+let isDone:boolean=true;
+let decVar:number=23;
+let myname:string = `Rajat Goel`;
+let fullname:string=` Hey, I'm  ${myname} . 
+and i'm ${decVar+2}`;
+let arr:number[]=[1,2,3,4];
+let newarr:Array<number>=[1,2,3,4];
+
+console.log("isDone" , isDone) ; 
+
+console.log("decVar" , decVar) ; 
+
+console.log("myname" , myname , "and fullName is \n\n" , fullname) ; 
+
+console.log("arr" , arr , "\n\n newarr" , newarr) ; 
